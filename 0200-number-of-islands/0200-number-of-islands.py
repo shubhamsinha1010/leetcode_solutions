708a945791540aada856ea586ed764862147b398
shubhamsinha1010 <62113279+shubhamsinha1010@users.noreply.github.com>
2023-04-07 15:38:19 +0530

    Time: 296 ms (71.24%), Space: 16.4 MB (79.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,36 +1,18 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        row = len(grid)
-        col = len(grid[0])
-        res = 0
-        
-        # Depth First Search Solution
-
-        # Use of another List[List] to mark all the islands as False visited
-        visited = [[False for i in range(col)] for j in range(row)]
-
-        # Thi method will mark the neighbour as visited and as well call the neighbours.
-        def neighbour(g,v,i,j):
-
-            if g[i][j] == "0" or v[i][j] == True:
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == '0':
                 return
-            
-            v[i][j] = True
-            if j - 1 >= 0:
-                neighbour(g,v,i,j-1)
-            if j + 1 < col:
-                neighbour(g,v,i,j+1)
-            if i - 1 >= 0:
-                neighbour(g,v,i-1,j)
-            if i + 1 < row:
-                neighbour(g,v,i+1,j)
+            grid[i][j] = '0'
+            dfs(i-1, j)
+            dfs(i+1, j)
+            dfs(i, j-1)
+            dfs(i, j+1)
 
-
-        # Looping over all the cells and if cell is already visited , continue else send it to neighbour method.
-
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] == '1'and visited[i][j] == False:
-                    neighbour(grid,visited,i,j)
-                    res += 1
-        return res
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    count += 1
+                    dfs(i, j)
+        return count
